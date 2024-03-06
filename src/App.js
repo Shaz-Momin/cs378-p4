2964f1f21bbe9c89c3c6c796373e9644ee5ba4a5
Shaz Momin <shaz.momin@utexas.edu>
2024-03-06 01:45:39 -0600

    added all games api call with frontend UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Card from "./components/Card";
+
+const options = {
+	method: "GET",
+	headers: {
+		"X-RapidAPI-Key": process.env.RAPID_API_KEY,
+		"X-RapidAPI-Host": "mmo-games.p.rapidapi.com",
+	},
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	let [games, setGames] = useState([]);
+
+	let getData = async () => {
+		let response = await fetch(
+			"https://mmo-games.p.rapidapi.com/games",
+			options
+		);
+		let data = await response.json();
+		setGames(data);
+	};
+
+	useEffect(() => {
+		getData();
+	}, []);
+
+	return (
+		<div className="App">
+			<h1 className="header">MMO Games Library</h1>
+			<div className="form">
+				<form>
+					<input
+						className="searchBar"
+						type="text"
+						placeholder="Lost Ark, New World, etc."
+						name="search"
+					/>
+					<input className="submitBtn" type="submit" value="Search" />
+				</form>
+			</div>
+			<div className="library">
+				{games &&
+					games.map((game, index) => {
+						return <Card game={game} key={index} />;
+					})}
+			</div>
+		</div>
+	);
 }
 
 export default App;
---
+++ src/components/Card.jsx
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from 'react'
+import '../Card.css'
+
+function Card({ game }) {
+
+    let [fullDate, setFullDate] = useState('')
+
+    useEffect(() => {
+        let monthDate = new Date(game.release_date).toString().split(' ').slice(1, 3).join(' ')
+        let year = new Date(game.release_date).toString().split(' ').slice(3, 4).join(' ')
+        setFullDate(monthDate + ', ' + year)
+    }, [])
+
+
+    return (
+        <div className='card'>
+            <h3>{game.title}</h3>
+            <img src={game.thumbnail} alt="Game cover" />
+            <div className='subheading'>
+                <div>{game.genre}</div>
+                <div>{game.platform}</div>
+            </div>
+            <p>{game.short_description}</p>
+            <div className="published">
+                <label>Published by</label>
+                <div className="extraInfo">{game.publisher}</div>
+            </div>
+            <div className="developed">
+                <label>Developed by</label>
+                <div className="extraInfo">{game.developer}</div>
+            </div>
+            <div className="releaseDate">
+                <label>Release date</label>
+                <div className="date">{fullDate}</div>
+            </div>
+            <div className='links'>
+                <a target="_blank" href={game.game_url}>Game Link</a>
+                <a target="_blank" href={game.profile_url}>Profile</a>
+            </div>
+        </div>
+    )
+}
+
+export default Card
